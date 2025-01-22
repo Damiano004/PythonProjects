28e87a570a7111e4504b6380b28e3277fc1122c7
Mael Ossanna Thimon <Mael.OssannaThimon@marconi.loc>
2025-01-22 16:46:15 +0100

    completato la parte di cryptamento dell'esercizio 2, manca la decriptazione e l'aggiunta di qualche commento UwU

--- Esercizio2/sub.py
+++ Esercizio2/sub.py
@@ -2,71 +2,113 @@
 from crypto_utils import LibCryptoError, ReadProcessingError, KeyImportError
 from crypto_utils import read_file, write_file
 
+def isCharacter(in_char: str) -> bool:
+     '''
+     Function that returns whether an input string
+     is an alphabetical letter or not
 
-# normal reference alphabet
-ALPH = 'abcdefghijklmnopqrstuvwxyz '
+     ### Parameters
+     - in_char: is the character that the function will check
 
-def substitute(in_str: str, ref_alph: str, perm_alph: str) -> str:
+     ### Returns
+     A boolean value where:
+     - True -> the input is an alphabetical letter
+     - False -> the input is not an alphabetical letter
+     '''
+     in_char = in_char.lower()
+     if ord(in_char)<97 | ord(in_char)>122:
+          return False
+     return True
+     
+def sumLetter(letter_a: str, letter_b:str) -> str:
+     '''
+     Function that adds the two letters and returns the
+     resulting letter, (letter_a + letter_b)
+
+     ### Parameters
+     - letter_a: the letter that gets summed by letter_b
+     - letter_b: the letter that summs letter_a
+
+     ### Returns
+     A letter that is the result between the sum of letter_a and letter_b
+     '''
+     ascii_a = ord(letter_a)
+     ascii_b = ord(letter_b)
+     offset = 0
+     if ascii_a-65 <=25:
+          offset = 65
+     else:
+        offset = 97
+     if ascii_b-65 <=25:
+          ascii_b -= 65
+     else:
+        ascii_b -= 97
+     ascii_a -= offset
+     ascii_a = (ascii_a + ascii_b) % 26
+     ascii_a += offset
+     return chr(ascii_a)
+               
+          
+
+
+def substitute(in_str: str, key: str) -> str:
     '''
     Function that performs the core substitution operation.
-    Can be inverted by simply swapping the input alphabets.
-    Assumes that the two alphabets contain the same characters,
-    with no repetitions.
     
     ### Parameters
     - in_str: is the string whose characters will be substituted
-    - ref_alph: is the reference alphabet
-    - perm_alph: is the permuted alphabet
+    - key: is the key that will be used to encrypt the plain text
     
     ### Returns
-    A string whose characters are the ones of in_str substituted
-    according to the alphabets.
+    A string containint the encrypted message.
 
     ---
     ## Raises
     LibCryptoError if in_str contains characters
-    not present in the alphabets.
+    that are not alphabetical characters.
     '''
     # initialize output
     out_str = ''
+    i = 0
     # substitute every character
     for char in in_str:
         # find position in reference alphabet
-        index = ref_alph.find(char)
-        if index < 0:
+        if not isCharacter(char):
             # character not found, raise error
             err_str = f'Error: message contains an invalid character: "{char}"'
             raise LibCryptoError(err_str)
         # append to output the corresponding character in the permuted alphabet
-        out_str += perm_alph[index]
+        out_str += sumLetter(char, key[i])
+        i += 1
     # return final string
     return out_str
 
-def check_key(key: str) -> None:
+def check_key(key: str, pt:str) -> None:
     '''
-    Function that checks the validity of a key, i.e. if it is a
-    permutation of the alphabet ALPH.
+    Function that checks the validity of a key, i.e. if it has the
+    same length as the plain text.
     
     ### Parameters
-    - key: string representing the permuted alphabet to check
+    - key: string representing the key used for encrypt the message
+    - pt: string representing the plain text
     
     ### Returns
     Nothing if the key is valid.
 
     ---
     ## Raises
-    KeyImportError if key is not a permutation of ALPH.
+    KeyImportError if key has not the same length as the pt.
     '''
     # initialize error string, will be completed
-    err_str = 'Error: key and alphabet '
+    err_str = 'Error: key and plain text '
     # check lengths
-    if len(key) != len(ALPH):
+    if len(key) != len(pt):
         err_str += 'do not have the same length'
         raise KeyImportError(err_str)
     # check that every character of ALPH is present in key
-    err_str += 'do not contain the same characters'
-    for char in ALPH:
-        if key.find(char) < 0:
+    err_str = 'Error: should only contain alphabetical letters'
+    for char in key:
+        if isCharacter(char):
             raise KeyImportError(err_str)
 
 
@@ -107,10 +149,6 @@ def import_key(raw_data: bytes) -> str:
     '''
     # this could raise an exception: desired behaviour
     key = import_trimmed_string(raw_data)
-    try:
-         check_key(key)
-    except KeyImportError as e:
-         raise ReadProcessingError(str(e))
     return key
 
 def encrypt():
@@ -130,8 +168,6 @@ def encrypt():
          default = 'key.txt',
          process = lambda raw: import_key(raw)
     )
-    # check validity
-    check_key(key)
     # read message
     pt, _ = read_file(
          subject = 'plaintext',
@@ -139,9 +175,11 @@ def encrypt():
          default = 'plaintext.txt',
          process = lambda raw: import_trimmed_string(raw)
     )
+    # check validity
+    #check_key(key)
     print('The plaintext is:\n', pt)
     # encrypt
-    ct = substitute(pt, ALPH, key)
+    ct = substitute(pt, key)
     # write ciphertext on file
     out_filename = write_file(
          data = ct.encode('utf-8'),
@@ -176,7 +214,7 @@ def decrypt():
     )
     print('The ciphertext is:\n', ct)
     # decrypt (inverting alph and key)
-    pt = substitute(ct, key, ALPH)
+    pt = substitute(ct, key)
     # write result
     print('The decrypted message is:\n', pt)
     out_filename = write_file(
@@ -211,3 +249,4 @@ while True:
             print('Invalid choice, please try again!')
     except LibCryptoError as e:
             print(e)
+
